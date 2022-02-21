a684089e690ff1cef0ddacbed97c1f72a6ef1e46
Luis Fernandez (Kayros) <luisalfonso634@gmail.com>
2022-02-21 15:50:34 -0300

    test_login5

--- users/views.py
+++ users/views.py
@@ -1,41 +1,59 @@
 """Users views."""
 
-from logging.handlers import DatagramHandler
-from urllib import request
 from django.contrib.auth import authenticate, login, logout
 from rest_framework import status, generics
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializers import UserSerializer
+<<<<<<< HEAD
 from django.utils.datastructures import MultiValueDictKeyError
 
 
 class LoginView(APIView):
+    
     def post(self, request):
-        data = request.data 
-        #if request.method == 'POST':
-        email = data.get('email', None)
-        password = data.get('password',None)
-        user = authenticate(email=email, password=password)
-        print("punto4")
-        print(user) #AQUI ESTA EL PROBLEMA
-        print("punto5")
-        if user is not None: 
-            if user.is_active:
-            #print("hola Miguel")
+        if request.method == 'POST':
+            print("punto1")
+            email = request.POST.get('email', False)
+            print(email)
+            password = request.POST.get('password',False)
+            print("punto3")
+            user = authenticate(request, email=email, password=password)
+            print("punto4")
+            print(user)
+            print("punto5")
+            if user: #AQUI ESTA EL PROBLEMA
+                #print("hola Miguel")
                 login(request, user)
                 print("punto6")
-                return Response(status=status.HTTP_200_OK)
+                return Response(UserSerializer(user).data,
+                status=status.HTTP_200_OK)
                 print("punto7")
             else:
                 print("punto8")
                 return Response(
                 status=status.HTTP_404_NOT_FOUND)
-        else:
-                print("punto9")
-                return Response(
-                status=status.HTTP_404_NOT_FOUND)
+=======
 
+
+class LoginView(APIView):
+    def post(self, request):
+        # Recuperamos las credenciales y autenticamos al usuario
+        email = request.data.get('email', None)
+        password = request.data.get('password', None)
+        user = authenticate(email=email, password=password)
+
+        # Si es correcto añadimos a la request la información de sesión
+        if user:
+            login(request, user)
+            return Response(
+                UserSerializer(user).data,
+                status=status.HTTP_200_OK)
+
+        # Si no es correcto devolvemos un error en la petición
+        return Response(
+            status=status.HTTP_404_NOT_FOUND)
+>>>>>>> 14d8ea251be4795ec19939cd38599e75b7c1f05c
 
 
 class LogoutView(APIView):
@@ -43,6 +61,14 @@ class LogoutView(APIView):
         # Borramos de la request la información de sesión
         logout(request)
 
+<<<<<<< HEAD
+class LogoutView(APIView):
+    def post(self, request):
+        # Borramos de la request la información de sesión
+        logout(request)
+
+=======
+>>>>>>> 14d8ea251be4795ec19939cd38599e75b7c1f05c
         # Devolvemos la respuesta al cliente
         return Response(status=status.HTTP_200_OK)
 
